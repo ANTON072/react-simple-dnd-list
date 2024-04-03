5b1b6b28828e38de5cfef435a1e39207650ed44a
Katsushi OUGI <ougi@to-r.net>
2024-04-03 21:55:19 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,8 @@
-import { useRef, createContext, useContext, useReducer, useState } from "react";
-
-interface DropResult {
-  source: number;
-  destination: number;
-}
-
-interface DropAction {
-  type: "setSource" | "setDestination";
-  payload: number;
-}
-
-interface AppContextValue {
-  $refs: React.MutableRefObject<Map<string, HTMLElement>>;
-  dispatch: React.Dispatch<DropAction>;
-  onDragEnd: () => void;
-}
-
-const AppContext = createContext<AppContextValue | null>(null);
-
-interface DraggableProps {
-  draggable: boolean;
-  onDragStart: (event: React.DragEvent) => void;
-  onDragEnter: (event: React.DragEvent) => void;
-  onDragOver: (event: React.DragEvent) => void;
-  onDragLeave: (event: React.DragEvent) => void;
-  onDragEnd: (event: React.DragEvent) => void;
-  onDrop: (event: React.DragEvent) => void;
-}
-
-type Provided = {
-  draggableProps: DraggableProps;
-};
-
-interface DraggableComponentProps {
-  children: (provided: Provided) => JSX.Element;
-  index: number;
-}
-
-interface DragDropContextComponentProps {
-  onDragEnd: (result: DropResult) => void;
-  children: React.ReactNode;
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import DragDropContext from "./DragDropContext";
+import Draggable from "./Draggable";
 
 function reorder<T>(list: T[], startIndex: number, endIndex: number): T[] {
   const result = [...list];
@@ -50,115 +12,34 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number): T[] {
   return result;
 }
 
-function reducer(state: DropResult, action: DropAction) {
-  switch (action.type) {
-    case "setSource":
-      return { ...state, source: action.payload };
-    case "setDestination":
-      return { ...state, destination: action.payload };
-    default:
-      return state;
-  }
-}
-
-function DragDropContext({
-  onDragEnd,
-  children,
-}: DragDropContextComponentProps) {
-  const $refs = useRef<AppContextValue["$refs"]["current"]>(new Map());
-
-  const [state, dispatch] = useReducer(reducer, {
-    source: -1,
-    destination: -1,
-  });
-
-  const handleDragEnd = () => {
-    if (state.source !== -1 && state.destination !== -1) {
-      onDragEnd(state);
-    }
-  };
-
-  return (
-    <AppContext.Provider
-      value={{
-        $refs,
-        dispatch,
-        onDragEnd: handleDragEnd,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
-}
-
-function Draggable({ children, index }: DraggableComponentProps) {
-  const context = useContext(AppContext);
-
-  const draggableProps = {
-    draggable: true,
-    onDragStart(event: React.DragEvent) {
-      event.dataTransfer.dropEffect = "move";
-      event.dataTransfer.effectAllowed = "move";
-      context?.dispatch({ type: "setSource", payload: index });
-    },
-    onDragEnter(event: React.DragEvent) {
-      event.preventDefault();
-    },
-    // 通過している要素のイベントが発火する
-    onDragOver(event: React.DragEvent) {
-      event.preventDefault();
-      const elm = context?.$refs.current.get(String(index));
-      if (!elm) return;
-      // カーソルが当たっている要素の相対位置情報を取得
-      const rect = elm.getBoundingClientRect();
-      // 要素内でのカーソル位置
-      const posY = event.clientY - rect.top;
-      // 要素の縦幅に対しての割合
-      const ratioY = Math.min(1, Math.max(0, posY / rect.height));
-      const destinationIndex = index + Math.round(ratioY);
-      context?.dispatch({ type: "setDestination", payload: destinationIndex });
-    },
-    onDragLeave(event: React.DragEvent) {
-      event.preventDefault();
-    },
-    onDragEnd() {
-      context?.onDragEnd();
-    },
-    onDrop(event: React.DragEvent) {
-      event.preventDefault();
-    },
-    ref(elm: HTMLElement) {
-      if (elm) {
-        context?.$refs.current.set(String(index), elm);
-      } else {
-        context?.$refs.current.delete(String(index));
-      }
-    },
-  };
-
-  return children({
-    draggableProps,
-  });
-}
-
 function App() {
   const [items, setItems] = useState([
-    { id: 1, name: "item1" },
-    { id: 2, name: "item2" },
-    { id: 3, name: "item3" },
+    { id: "id_a", name: "item1" },
+    { id: "id_b", name: "item2" },
+    { id: "id_c", name: "item3" },
   ]);
 
   return (
     <DragDropContext
       onDragEnd={(result) => {
-        const newOrder = reorder(items, result.source, result.destination);
+        // TODO: ここに前のやつが入ってる！！
+        console.log("result", result);
+        const newOrder = reorder(
+          items,
+          result.sourceIndex,
+          result.destinationIndex
+        );
         setItems(newOrder);
       }}
     >
       <ul>
         {items.map((item, index) => (
-          <Draggable key={item.id} index={index}>
-            {(provided) => <li {...provided.draggableProps}>{item.name}</li>}
+          <Draggable key={item.id} draggableId={item.id} index={index}>
+            {(provided) => (
+              <li className={item.id} {...provided.draggableProps}>
+                {item.name}
+              </li>
+            )}
           </Draggable>
         ))}
       </ul>
---
+++ src/DragDropContext.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useReducer, useRef } from "react";
+import {
+  ActionType,
+  AppContext,
+  AppContextValue,
+  ReducerState,
+} from "./context";
+
+interface Props {
+  children: React.ReactNode;
+  onDragEnd: (result: ReducerState) => void;
+}
+
+function reducer(state: ReducerState, action: ActionType) {
+  switch (action.type) {
+    case "SET_DESTINATION_INDEX":
+      return { ...state, destinationIndex: action.payload };
+    case "SET_SOURCE_INDEX":
+      return { ...state, sourceIndex: action.payload };
+    default:
+      return state;
+  }
+}
+
+function DragDropContext({ children, onDragEnd }: Props) {
+  const $refs = useRef<AppContextValue["$refs"]["current"]>(new Map());
+
+  const [store, dispatch] = useReducer(reducer, {
+    destinationIndex: -1,
+    sourceIndex: -1,
+  });
+
+  const handleDragEnd = () => {
+    onDragEnd({
+      destinationIndex: store.destinationIndex,
+      sourceIndex: store.sourceIndex,
+    });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        $refs,
+        dispatch,
+        onDragEnd: handleDragEnd,
+        store,
+      }}
+    >
+      {children}
+      <div>
+        <p>Source: {store.sourceIndex}</p>
+        <p>Destination: {store.destinationIndex}</p>
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default DragDropContext;
